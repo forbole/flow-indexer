f99e77cffd7be30a2055546671e81b6e2c71f337
Kwun Yeung <kwun@forbole.com>
2020-11-30 16:27:58 +0000

    update account address conversion

--- src/accounts/accounts.ts
+++ src/accounts/accounts.ts
@@ -1,17 +1,12 @@
 import { AccessAPI, GetAccountRequest, GetAccountResponse } from "@onflow/protobuf"
 import { unary } from "../utils/unary"
-
-const u8ToHex = u8 => Buffer.from(u8).toString("hex")
-const paddedHexBuffer = (hex, pad) =>
-  Buffer.from(hex.padStart(pad * 2, 0), "hex")
-
-const addressBuffer = addr => paddedHexBuffer(addr, 8)
+import { addressBuffer } from "../utils/address"
 
 export const GetAccount = async (address: string):Promise<void> => {
     try{
         const req = new GetAccountRequest()
-        console.log(addressBuffer(address))
-        req.setAddress(paddedHexBuffer(Buffer.from(address).toString('hex'),8))
+        console.log(addressBuffer(Buffer.from(address, 'base64').toString('hex')))
+        req.setAddress(addressBuffer(Buffer.from(address, 'base64').toString('hex')))
         console.log("Get account address: %o", address)
         const res:GetAccountResponse = await unary(AccessAPI.GetAccount, req)
         console.log(res)
